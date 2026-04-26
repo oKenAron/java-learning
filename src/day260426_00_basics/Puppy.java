6eb42d6be96bf85241b612f334b57e80e1b334ba
oKenAron <wu.naqin.uz@tut.jp>
2026-04-26 19:27:09 +0900

    change: change Puppy.java to fix Warning.

--- src/day260426_00_basics/Puppy.java
+++ src/day260426_00_basics/Puppy.java
@@ -1,10 +1,23 @@
 package day260426_00_basics;
 
 public class Puppy {
-    public Puppy(String name){
-        System.out.println("小狗的名字是：" + name );
+    private String puppyName;
+    public Puppy(String name) {
+        this.puppyName = name;
+        System.out.println("小狗的名字是：" + this.puppyName);
     }
-    public static void main(String[] args){
-        Puppy myPuppy = new Puppy("Tommy");
+    public void puppyBark(){
+        System.out.println("指挥小狗叫.\n"+this.puppyName+":Bark!!!");
+    }
+    public void renamePuppy(String name){
+        System.out.print("重命名小狗 "+this.puppyName);
+        this.puppyName = name;
+        System.out.println(" 为："+this.puppyName);
+    }
+    public static void main(String[] args) {
+        Puppy barkingPuppy = new Puppy("Tommy");
+        barkingPuppy.puppyBark();
+        barkingPuppy.renamePuppy("Trump");
+        barkingPuppy.puppyBark();
     }
 }
