0fb82fdfec913ddc2c7132d6302121a40fc5dca4
oKenAron <wu.naqin.uz@tut.jp>
2026-04-26 18:39:45 +0900

    chore: add Hospital.java as remake of BankQueueSystem.java

---
+++ Chapter01_Basics/Hospital.java
@@ -0,0 +1,40 @@
+class HosTicket {
+    private final int ticketNumber;
+    private final String patientName;
+    private final int patientAge;
+
+    public HosTicket(int number, String name, int age){
+        this.ticketNumber = number;
+        this.patientName = name;
+        this.patientAge = age;
+    }
+
+    public int getTicketInfo(int number){
+        return this.ticketNumber;
+    }
+}
+
+class HospitalMachine {
+    public static int totalTicket = 0;
+    public static int currentTicket = 0;
+
+    public static HosTicket generateTicket(String name, int age){
+        totalTicket++;
+        HosTicket patientTicket = new HosTicket(totalTicket, name, age);
+        return patientTicket;
+    }
+
+    public static void callNextPatient(){
+        currentTicket++;
+        System.out.println("请"+currentTicket+"号患者就诊");
+    }
+}
+
+
+
+public class Hospital {
+    public static void main(String[] args) {
+
+        System.out.println("tbd");
+    }
+}
