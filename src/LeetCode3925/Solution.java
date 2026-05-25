818905085c52fec1f46e1d44c7422a22e01e11c6
oKenAron <wu.naqin.uz@tut.jp>
2026-05-25 19:30:21 +0900

    refactor: [3925] 提取变量 nums.length

--- src/LeetCode3925/Solution.java
+++ src/LeetCode3925/Solution.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 class Solution {
     public int[] concatWithReverse(int[] nums) {
-        int[] ans = new int[2 * nums.length];
-        for (int i = 0; i < nums.length; i++){
-            ans[i] = ans[2 * nums.length - 1 - i] = nums[i];
+        // 提取变量 nums.length
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++){
+            ans[i] = ans[2 * n - 1 - i] = nums[i];
         }
         return ans;
     }
