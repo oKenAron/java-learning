a31fe87840b953d49ca0d4be376ca9c476a3f448
oKenAron <wu.naqin.uz@tut.jp>
2026-05-30 19:51:50 +0900

    perf: [1] 引入 HashMap 构建值到下标的逆向索引，实现 O(N) 求解

--- src/LeetCode1/Solution.java
+++ src/LeetCode1/Solution.java
@@ -1,16 +1,16 @@
 package LeetCode1;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
-        for (int i = 0; i < len - 1; i++) {
-           for (int j = i + 1; j < len; j++){
-               if (nums[i] + nums[j] == target){
-                   return new int[]{i, j};
-               }
-           }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No Two Sum Solution");
     }
@@ -18,8 +18,8 @@ class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        System.out.println(Arrays.toString(sol.twoSum(new int[]{2,7,11,5}, 9)));
-        System.out.println(Arrays.toString(sol.twoSum(new int[]{3,2,4}, 6)));
-        System.out.println(Arrays.toString(sol.twoSum(new int[]{3,3}, 6)));
+        System.out.println(Arrays.toString(sol.twoSum(new int[]{2, 7, 11, 5}, 9)));
+        System.out.println(Arrays.toString(sol.twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(sol.twoSum(new int[]{3, 3}, 6)));
     }
 }
