6f09c4d802b1e4e8e5779f3d927f449f15817c21
oKenAron <wu.naqin.uz@tut.jp>
2026-05-31 17:07:27 +0900

    perf: [169] 引入摩尔投票法 (Boyer-Moore)，实现 O(1) 空间复杂度的最优解

--- src/LeetCode169/Solution.java
+++ src/LeetCode169/Solution.java
@@ -4,21 +4,19 @@ import java.util.HashMap;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums){
-            int currentCount = map.getOrDefault(num, 0) + 1;
-            map.put(num, currentCount);
-            if (currentCount > nums.length / 2){
-                return num;
-            }
+        int count = 0, currentBoss = 0;
+        for (int num : nums) {
+            if (count == 0) currentBoss = num;
+            if (num == currentBoss) count++;
+            else count--;
         }
-        throw new IllegalArgumentException("No majority Element");
+        return currentBoss;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solution sol = new Solution();
 
-        System.out.println(sol.majorityElement(new int[]{3,2,3}));
-        System.out.println(sol.majorityElement(new int[]{2,2,1,1,1,2,2}));
+        System.out.println(sol.majorityElement(new int[]{3, 2, 3}));
+        System.out.println(sol.majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}));
     }
 }
